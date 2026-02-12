54d40a43bd6af277f57120e196ccb04d1ec91711
cybogz <cyruscbcbcb93@hotmail.com>
2026-02-12 07:50:10 +0000

    added more code to ifTesting.py

--- pyPractice/ifTesting/ifTesting2.py
+++ pyPractice/ifTesting/ifTesting2.py
@@ -1,4 +1,4 @@
-age = 15
+age = 20
 
 if age >= 18:
     print("You are old enough to vote")
@@ -6,3 +6,10 @@ else:
     print("You are not old enough to vote")
     yearsTillVoting = 18-age
     print("You need " + str(yearsTillVoting) + " years until you can vote")
+
+if age < 4:
+    print("Admission is free")
+elif age > 18:
+    print("You must pay $10")
+else:
+    print("You must pay $5")
