71998bbfa4accf39fc85c3fd364c1e88558019c9
cybogz <cyruscbcbcb93@hotmail.com>
2026-02-12 09:14:01 +0000

    added a new file called ifTesting3.py

---
+++ pyPractice/ifTesting/ifTesting3.py
@@ -0,0 +1,8 @@
+toppings = ["pepperoni", "cheese"]
+
+if "pepperoni" in toppings:
+    print("Your topping has been added")
+if "cheese" in toppings:
+    print("Your topping has been added")
+if "pineapple" in toppings:
+    print("This is not available")
