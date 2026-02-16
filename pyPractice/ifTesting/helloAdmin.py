7935ac80088193c05365e41c58493947eab7b70c
cybogz <cyruscbcbcb93@hotmail.com>
2026-02-16 08:54:53 +0000

    modified the helloAdmin.py file by adding an if statement to check if the list was empty or not. if empty it would alert the user

--- pyPractice/ifTesting/helloAdmin.py
+++ pyPractice/ifTesting/helloAdmin.py
@@ -1,7 +1,11 @@
-usernames = ["cybogz", "chivajavy", "admin", "stevenator", "jermainiamx"]
+usernames = []
 
-for users in usernames:
-    if users == "admin":
-        print("Hello admin, would you like to see a status report?")
-    else:
-        print("Hello " + users.title() + " thank you for logging in again")
+if usernames:
+    for users in usernames:
+        if users == "admin":
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print("Hello " + users.title() + " thank you for logging in again")
+else:
+    print("We need to find some users")
+
