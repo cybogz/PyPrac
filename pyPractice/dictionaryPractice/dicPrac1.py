eca4dc762b2ece4ed98c5a7fcad33e75fa93c5db
cybogz <cyruscbcbcb93@hotmail.com>
2026-02-21 08:57:50 +0000

    modifing the code to add to the x direction and also deleted a key from the dictionary

--- pyPractice/dictionaryPractice/dicPrac1.py
+++ pyPractice/dictionaryPractice/dicPrac1.py
@@ -1,4 +1,14 @@
-alien_0 = {'color': 'green', 'points': 5}
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'fast'}
+print("This is the original x position: " + str(alien_0['x_position']))
 
-print(alien_0['color'])
-print(alien_0['points'])
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print("This is your new x position: " + str(alien_0['x_position']))
