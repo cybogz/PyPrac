b2383f7f86acafe140a06508dd07d7993c0b4d8e
cybogz <cyruscbcbcb93@hotmail.com>
2026-02-24 09:09:32 +0000

    worked on first practice problem

---
+++ pyPractice/dictionaryPractice/person.py
@@ -0,0 +1,7 @@
+myFriend = {'firstName': 'Guillermo', 'lastName': 'Lopez',
+            'age': 32, 'city': 'San Diego'}
+
+print(myFriend['firstName'])
+print(myFriend['lastName'])
+print(myFriend['age'])
+print(myFriend['city'])
