109ffc7bd548d8203184b9265be1c78652d9ed64
cybogz <cyruscbcbcb93@hotmail.com>
2026-03-11 19:50:35 +0000

    adding changes to aliens.py file

--- pyPractice/dictionaryPractice/aliens.py
+++ pyPractice/dictionaryPractice/aliens.py
@@ -6,6 +6,12 @@ for alienNumber in range(30):
     alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(alien)
 
+for changeColor in aliens[0:3]:
+    if changeColor['color'] == 'green':
+        changeColor['color'] = 'yellow'
+        changeColor['points'] = 10
+        changeColor['speed'] = 'medium'
+
 #show the first 5 aliens
 for displayAlien in aliens[:5]:
     print(displayAlien)
