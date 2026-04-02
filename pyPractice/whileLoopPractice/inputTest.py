ed715bba1514fe4a5e04d13102e3fa38667ca80b
cybogz <cyruscbcbcb93@hotmail.com>
2026-04-02 19:47:15 +0000

    Adding a new folder and file to practice user input and while loops

---
+++ pyPractice/whileLoopPractice/inputTest.py
@@ -0,0 +1,6 @@
+prompt_message = "I would like your name in order to get my greeting"
+prompt_message += "\nCan you please enter your name: "
+
+name = input(prompt_message)
+
+print("Hello " + name)
