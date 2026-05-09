3a438dc25851002dbdb9fe6e1bc153c1513426d7
cybogz <cyruscbcbcb93@hotmail.com>
2026-05-09 19:18:42 +0000

    added a more efficiant for loop in the magician.py script to show magicians names

--- pyPractice/functions/magicians.py
+++ pyPractice/functions/magicians.py
@@ -2,16 +2,15 @@ def show_magicians(magician_list):
      for magician in magician_list:
           print(magician)
 
-def make_great(magicians_to_be_modified, new_magician_list):
-        while magicians_to_be_modified:
-          name = magicians_to_be_modified.pop()
-          new_entry = name + " the great"
-          new_magician_list.append(new_entry)
+def make_great(magicians_to_be_modified):
+        """Adding text to each index in the list"""
+        for person in range(len(magicians_to_be_modified)):
+              magicians_to_be_modified[person] += " the great"
           
           
 
 magician_names = ["houdini", "criss angel", "david blaine", "js magic"]
-new_magician_message = []
+#new_magician_message = []
 
-make_great(magician_names, new_magician_message)
-show_magicians(new_magician_message)
+make_great(magician_names)
+show_magicians(magician_names)
