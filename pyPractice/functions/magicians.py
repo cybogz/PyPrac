0b135f8a4afeb90d98bec2a5a54a6cdde56fcc2d
cybogz <cyruscbcbcb93@hotmail.com>
2026-05-10 04:27:50 +0000

    changed the code to pass in a copy of the list instead of the actual list

--- pyPractice/functions/magicians.py
+++ pyPractice/functions/magicians.py
@@ -3,14 +3,17 @@ def show_magicians(magician_list):
           print(magician)
 
 def make_great(magicians_to_be_modified):
-        """Adding text to each index in the list"""
-        for person in range(len(magicians_to_be_modified)):
-              magicians_to_be_modified[person] += " the great"
+    new_list = []
+
+    for mag in magicians_to_be_modified:
+         new_list.append(mag + " is the best")
+    return new_list
           
           
 
 magician_names = ["houdini", "criss angel", "david blaine", "js magic"]
 #new_magician_message = []
 
-make_great(magician_names)
-show_magicians(magician_names)
+newList = make_great(magician_names[:])
+show_magicians(newList)
+#show_magicians(magician_names)
