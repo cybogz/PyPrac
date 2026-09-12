9edcf826b59718b713a00cbed58e966d2e677c6b
cybogz <cyruscbcbcb93@hotmail.com>
2026-09-12 23:34:36 +0000

    Refactored the code and added functions to organize the code

--- pyPractice/log_analyzer_tool/log_analyzer.py
+++ pyPractice/log_analyzer_tool/log_analyzer.py
@@ -1,37 +1,64 @@
-filename = "app.log"
+#
 
-info_count = 0
-warning_count = 0
-error_count = 0
+def message_count(filename):
 
-info_name = "INFO"
-warning_name = "WARNING"
-error_name = "ERROR"
+    info_name = "INFO"
+    warning_name = "WARNING"
+    error_name = "ERROR"
 
-error_messages = []
+    info_count = 0
+    warning_count = 0
+    error_count = 0
 
-with open(filename) as file_object:
-    for line in file_object:
-        if info_name in line:
-            info_count += 1
+    with open(filename) as file_object:
+        for line in file_object:
+            if info_name in line:
+                info_count += 1
 
-        elif warning_name in line:
-            warning_count += 1
+            elif warning_name in line:
+                warning_count += 1
 
-        elif error_name in line:
-            error_count += 1
-            error_messages.append(line.strip())
+            elif error_name in line:
+                error_count += 1
 
+    return info_count, warning_count, error_count
 
-print("\nError log count")
-print("---------------")
+def get_error_messages(filename):
 
-print(f"Number of INFO messages: {info_count}")
-print(f"Number of WARNING messages: {warning_count}")
-print(f"Number of ERROR messages: {error_count}")
+    error_messages = []
 
-print("\nError messages")
-print("---------------")
+    with open(filename) as file_object:
+        for error in file_object:
+            if "ERROR" in error:
+                error_messages.append(error.strip())
 
-for error in error_messages:
-    print(error)
+    return error_messages
+
+def display_information(info_count, warning_count, error_count, display_error):
+
+    print("\n*Error Log Count*")
+    print("---------------")
+
+    print(f"Number of INFO messages: {info_count}")
+    print(f"Number of WARNING messages: {warning_count}")
+    print(f"Number of ERROR messages: {error_count}")
+
+    print("\n*Error Messages*")
+    print("---------------")
+
+    for error in display_error:
+        print(error)
+
+def main():
+
+    filename = "app.log"
+
+    info_count, warning_count, error_count = message_count(filename)
+    display_error = get_error_messages(filename)
+    display_information(info_count, warning_count, error_count, display_error)
+
+main()
+#error_messages.append(line.strip())
+
+#for error in error_messages:
+    #print(error)
