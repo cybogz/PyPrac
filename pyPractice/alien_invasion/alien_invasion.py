0049fd6326b3adafdf3b4bc952739e1b9c07d702
cybogz <cyruscbcbcb93@hotmail.com>
2026-09-23 01:15:07 +0000

    Added group of bullets to the main loop

--- pyPractice/alien_invasion/alien_invasion.py
+++ pyPractice/alien_invasion/alien_invasion.py
@@ -1,7 +1,9 @@
 import pygame
+import game_functions as gf
+
 from settings import Settings
 from ship import Ship
-import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     # Initialize game and create screen object
@@ -13,11 +15,15 @@ def run_game():
     # make a ship
     ship = Ship(ai_settings, screen)
 
+    # make a group to store bullets in
+    bullets = Group()
+
     #Start the main loop for the game
     while True:
 
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update() # The ships position will update after keyboard events and before screen update
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update() # calls bullet.update() for each bullet we place in the group bullets
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
